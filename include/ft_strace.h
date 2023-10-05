e456d672f87df0db84dd2b3e3b40b50ec63759de
tsiguenz <tsiguenz@student.42.fr>
2023-10-05 18:49:31 +0200

    wip list syscall codes and parameters

--- include/ft_strace.h
+++ include/ft_strace.h
@@ -1,11 +1,21 @@
 #ifndef FT_STRACE_H
 #define FT_STRACE_H
 
+#include <errno.h>
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <sys/ptrace.h>
 #include <sys/types.h>
+#include <sys/user.h>
 #include <sys/wait.h>
 #include <unistd.h>
+
+#define FATAL(...)                                                             \
+  do {                                                                         \
+    fprintf(stderr, __VA_ARGS__);                                              \
+    exit(1);                                                                   \
+  } while (0)
 
 #endif  // FT_STRACE_H
--- src/main.c
+++ src/main.c
@@ -1,21 +1,37 @@
 #include <ft_strace.h>
 
-int main(void) {
+const char *prog_name;
+
+int main(int argc, char **argv) {
+  prog_name = argv[0];
+  if (argc != 2)
+    FATAL("Bad number of arguments\n");
   pid_t child = fork();
-  if (child < 0) {
-    fprintf(stderr, "fork() error\n");
-    return EXIT_FAILURE;
-  }
+  if (child < 0)
+    FATAL("%s\n", strerror(errno));
   if (child == 0) {
     ptrace(PTRACE_TRACEME, 0, NULL, NULL);
-    execl("/bin/ls", "/bin/ls", NULL);
-  } else {
-    if (wait(NULL) == -1) {
-      fprintf(stderr, "wait() error\n");
-      return EXIT_FAILURE;
-    }
-    ptrace(PTRACE_SEIZE, child, 0, 0);
+    execve(argv[1], argv + 1, NULL);
+    FATAL("%s\n", strerror(errno));
   }
+  struct user_regs_struct regs;
+  int64_t                 syscall;
+  ptrace(PTRACE_SETOPTIONS, child, 0, PTRACE_O_EXITKILL);
+  while (42) {
+    ptrace(PTRACE_SYSCALL, child, 0, 0);
+    if (waitpid(child, 0, 0) == -1)
+      FATAL("waitpid()1 fail\n");
+    ptrace(PTRACE_GETREGS, child, 0, &regs);
+    syscall = regs.orig_rax;
+    fprintf(stderr, "%ld(%lld, %lld, %lld, %lld, %lld, %lld)", syscall,
+            regs.rdi, regs.rsi, regs.rdx, regs.r10, regs.r8, regs.r9);
 
+    // syscall out
+    ptrace(PTRACE_SYSCALL, child, 0, 0);
+    if (waitpid(child, 0, 0) == -1)
+      FATAL("waitpid()2 fail\n");
+    if (ptrace(PTRACE_GETREGS, child, 0, &regs) == -1)
+      fprintf(stderr, " = %lld\n", regs.rax);
+  }
   return EXIT_SUCCESS;
 }
